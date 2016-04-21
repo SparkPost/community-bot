cf21efa773ec28efa2f1f7ea01afebd4058c4112
Cole Furfaro-Strode <colestrode@gmail.com>
2016-04-21 15:23:57 -0400

    adds welcome message

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+'use strict';
+
+let fs = require('fs');
+
 require('skellington')({
   storage: require('botkit-storage-mongo')({mongoUri: process.env.MONGOLAB_URI}),
   plugins: [
     require('sk-nirvana'),
-    require('sk-sparkpost-webhook')
+    require('sk-sparkpost-webhook'),
+    require('skellington-welcome')({
+      type: 'post',
+      title: 'Welcome to the SparkPost Community!',
+      text: fs.readFileSync('./welcome.md')
+    })
   ]
 });
